f35c107a9c8a5e114478a8308e8ddac1d0339233
emilywin825 <emilywin825@naver.com>
2023-09-09 05:24:23 +0900

    github-action test

---
+++ src/test/java/com/kusitms/partcrossstudy/PartcrossstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kusitms.partcrossstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartcrossstudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
